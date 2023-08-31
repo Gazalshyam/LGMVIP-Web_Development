51b83a18c60b317f776f2c1c1a1852074ec60e2e
Gazal Shyam <gazalshyam18@gmail.com>
2023-08-31 22:09:02 +0530

    Adding-task

---
+++ TASK_REACT_APP/reactapp/src/Contact.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+            <h2>Let's catch up!</h2>
+            <p>It's nice that you got here. I am the author of this website :) </p>
+            <p>I am open to work  - please contact with me via email: gazalshyam18@gmail.com or <a href="https://pl.linkedin.com/in/gazal-shyam">LinkedIn</a>.</p>
+
+            <img src="https://user-images.githubusercontent.com/56108700/141132453-045018b0-7d37-437a-a690-c4d53579d52a.jpg" alt="pic"></img>
+            </div>
+        );
+    }
+}
+
+export default Contact;
---
+++ TASK_REACT_APP/reactapp/src/Home.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class Home extends Component {
+    render() {
+        return (
+
+            <div>
+                <h2>WELCOME!</h2>
+                <p>My name is Gazal Shyam I am a student in the department of Computer Science at the National Institute of Technology Hamirpur. </p>
+                    <p> I would like to invite you to visit my profile on  <a href="https://pl.linkedin.com/in/gazal-shyam">LinkedIn</a> and <a href="https://github.com/gazalshyam18">Github</a>.</p>
+                    <p> I am open to work  - please contact with me via email:gazalshyam18@gmail.com</p>
+            </div>
+        );
+    }
+}
+export default Home;
